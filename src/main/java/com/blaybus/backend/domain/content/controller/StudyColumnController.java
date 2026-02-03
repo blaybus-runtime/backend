c0b98497cf06fcfe11b7d37546233bf886655447
KimDongSu <kimdongsu481@gmail.com>
2026-02-03 13:48:53 +0900

    comment: StudyColumnController에 api 설명 주석 ã 추가

--- src/main/java/com/blaybus/backend/domain/content/controller/StudyColumnController.java
+++ src/main/java/com/blaybus/backend/domain/content/controller/StudyColumnController.java
@@ -20,6 +20,12 @@ public class StudyColumnController {
 
     private final StudyColumnService studyColumnService;
 
+    /*
+    /api/v1/columns/recent?limit=5 를 처리하는 api 입니다.
+    limit로 전달한 개수만큼 최근 칼럼을 조회해서 반환합니다. 파라미터 미지정시 5개만 가져와서 반환합니다.
+    홈 화면에 칼럼을 넣는 용도로 설계한 api라서 정보를 요약해서 전달했습니다. 전달 형식은 api 명세를 참고해주세요.
+    - 김동수 -
+    */
     @GetMapping("/recent")
     public ResponseEntity<Map<String, Object>> getRecentColumns(
             @RequestParam(defaultValue = "5") int limit
