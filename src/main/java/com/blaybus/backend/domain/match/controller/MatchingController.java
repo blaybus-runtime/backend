4844bbfd648044a64cb833d65480eb345aebafa0
KimDongSu <kimdongsu481@gmail.com>
2026-02-04 16:30:14 +0900

    feat: /matchings?mentorId=1&date=2020-02-02 api 멘토만 호출할 수 있게 수정

--- src/main/java/com/blaybus/backend/domain/match/controller/MatchingController.java
+++ src/main/java/com/blaybus/backend/domain/match/controller/MatchingController.java
@@ -3,8 +3,11 @@ package com.blaybus.backend.domain.match.controller;
 import com.blaybus.backend.domain.match.dto.response.MenteeCardResponse;
 import com.blaybus.backend.domain.match.service.MatchingService;
 import com.blaybus.backend.global.dto.ApiResponse;
+import com.blaybus.backend.global.enum_type.Role;
+import com.blaybus.backend.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,10 +25,16 @@ public class MatchingController {
 
     @GetMapping
     public ApiResponse<List<MenteeCardResponse>> getMyMentees(
-            @RequestParam Long mentorId,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate date
     ) {
-        List<MenteeCardResponse> response = matchingService.getMyMentees(mentorId, date);
+        if(userDetails.getRole() != Role.MENTOR) {
+            throw new IllegalArgumentException("멘토만 접근할 수 있습니다.");
+        }
+
+        Long currentUserId = userDetails.getUserId();
+
+        List<MenteeCardResponse> response = matchingService.getMyMentees(currentUserId, date);
         return ApiResponse.onSuccess(response);
     }
 }
