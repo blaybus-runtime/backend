a9658f847092642f65d457808ddf2c4345ac0339
KimDongSu <kimdongsu481@gmail.com>
2026-02-07 14:56:48 +0900

    feat: 댓글 관련 글로벌 에러 추가

--- src/main/java/com/blaybus/backend/global/response/status/ErrorStatus.java
+++ src/main/java/com/blaybus/backend/global/response/status/ErrorStatus.java
@@ -20,7 +20,10 @@ public enum ErrorStatus {
     _TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "TASK4001", "해당 할 일을 찾을 수 없습니다."),
     _PLANNER_NOT_FOUND(HttpStatus.NOT_FOUND, "PLANNER4001", "해당 플래너를 찾을 수 없습니다."),
 
-    _WORKSHEET_NOT_FOUND(HttpStatus.NOT_FOUND, "TASK4002", "다운로드할 학습지가 없습니다.");
+    _WORKSHEET_NOT_FOUND(HttpStatus.NOT_FOUND, "TASK4002", "다운로드할 학습지가 없습니다."),
+
+    _COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT4001", "해당 댓글을 찾을 수 없습니다."),
+    _COMMENT_NOT_WRITER(HttpStatus.FORBIDDEN, "COMMENT4002", "댓글 작성자만 수정/삭제할 수 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
