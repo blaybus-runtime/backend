8e42273bf3143b45a9c179952ff7e27495a794c0
KimDongSu <kimdongsu481@gmail.com>
2026-02-08 13:31:06 +0900

    feat: 알림 유형 나타내는 Enum 생성

---
+++ src/main/java/com/blaybus/backend/global/enum_type/NotificationType.java
@@ -0,0 +1,9 @@
+package com.blaybus.backend.global.enum_type;
+
+public enum NotificationType {
+    NEW_TODO,       // 멘토가 할 일 추가
+    NEW_WORKSHEET,  // 학습지 등록
+    NEW_COMMENT,    // 댓글 달림
+    SUBMISSION,     // 멘티가 과제 제출
+    FEEDBACK_REMINDER // 피드백 재촉 (스케줄러)
+}
